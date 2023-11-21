253318990038473fb0cb5cc2cd7bd015534ad364
Shreyas Patil <patil.shreyas@northeastern.edu>
2023-11-20 21:50:25 -0500

    initial commit ingredient

--- chef-2-go-backend/routes/index.js
+++ chef-2-go-backend/routes/index.js
@@ -1,6 +1,8 @@
 // Importing the Recipe route
 import recipeRouter from "./recipe-route.js";
+import ingredientRouter from "./ingredient-route.js";
 
 export default (app) => {
-  app.use("/recipes", recipeRouter);
+  app.use("/recipies", recipeRouter);
+  app.use("/ingredient", ingredientRouter);
 };
