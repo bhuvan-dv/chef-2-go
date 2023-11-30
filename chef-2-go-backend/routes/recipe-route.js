7236ab4d63d68e462fc120e69f9de33932aa3c8d
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-11-30 15:26:51 -0500

    routes added for comments

--- chef-2-go-backend/routes/recipe-route.js
+++ chef-2-go-backend/routes/recipe-route.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 // Importing the Recipe Controller
 import * as recipeController from '../controllers/recipe-controller.js';
+import * as commentController from '../controllers/comment-controller.js';
 
 const router = express.Router();
 router.route('/')
@@ -12,5 +13,13 @@ router.route('/:id')
    .get(recipeController.get)
    .put(recipeController.put)
    .delete(recipeController.remove);
+
+router.route('/:id/comments')
+   .post(commentController.post);
+
+router.route('/:id/comments/:id')
+   .get(commentController.find)
+   .delete(commentController.remove)
+   .put(commentController.put);
 
 export default router;
