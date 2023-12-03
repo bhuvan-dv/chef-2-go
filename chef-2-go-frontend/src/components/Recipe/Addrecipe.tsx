519fd15fc2f667783d84829fec509bd5a8132ec0
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-03 18:30:13 -0500

    created Recipe and Addrecipe component

---
+++ chef-2-go-frontend/src/components/Recipe/Addrecipe.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Addrecipe = () => {
+  return (
+    <div>Addrecipe</div>
+  )
+}
+
+export default Addrecipe
---
+++ chef-2-go-frontend/src/pages/Recipe/RecipeHome.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Recipe from '../../components/Recipe/Recipe'
+const RecipeHome = () => {
+  return (
+    <div>
+        <Recipe/>
+    </div>
+  )
+}
+
+export default RecipeHome
