c6cee5c04f01a2d2436fe8d4c93511a2144ffdc7
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-03 18:48:11 -0500

    added Morion font

--- chef-2-go-frontend/tailwind.config.js
+++ chef-2-go-frontend/tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      transitionTimingFunction: {
+        'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+        'out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+      },
       backgroundColor: {
         'vanilla-cream': '#FFF5E1',
         'soft-mint-green': "#C5E1A5",
