eeaf9e9742c685143bdf314c24de3de8c5eb9f58
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-07 00:18:44 -0500

    created i18n config files and wrapped app comp

--- chef-2-go-frontend/src/index.tsx
+++ chef-2-go-frontend/src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from './i18n';
+import { I18nextProvider } from 'react-i18next';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
     <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
