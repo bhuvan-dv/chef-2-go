9a5a0a4c3de129be5fb49fa92aeeca5676ccd66b
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-10 10:47:57 -0500

    topbar component modified

--- chef-2-go-frontend/src/components/admin/menu/Topbar.tsx
+++ chef-2-go-frontend/src/components/admin/menu/Topbar.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Box, IconButton, useTheme } from '@mui/material';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../theme';
+
 
 const Topbar: React.FC = () => {
+    const theme=useTheme();
+    const colors=tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
     return (
-        <div>Topbar</div>
+        <Box display="flex" justifyContent="space-between" p={2}>
+            <Box display="flex" 
+            backgroundColor={colors.primary[400]} 
+            borderRadius="3px" >
+
+            </Box>
+            <IconButton sx={{display:"flex"}}></IconButton>
+        </Box>
     );
 };
 
