c722272e8ce2a3ef1cfd26c3f67dc132ae1f9ced
Keerthana Mikkili <mikkili.k@northeastern.edu>
2023-12-09 23:01:06 -0500

    Added Chef route

--- chef-2-go-backend/routes/index.js
+++ chef-2-go-backend/routes/index.js
@@ -4,10 +4,12 @@ import recipeRouter from "./recipe-route.js";
 import userRouter from "./user-route.js";
 import searchRouter from "./search-route.js";
 import ingredientRouter from "./ingredient-route.js";
+import chefrouter from "./chef-route.js";
 
 export default (app) => {
   app.use("/users",userRouter);
   app.use("/recipes", recipeRouter);
   app.use("/search",searchRouter);
   app.use("/ingredient", ingredientRouter);
+  app.use("/chef", chefrouter)
 };
--- chef-2-go-backend/routes/user-route.js
+++ chef-2-go-backend/routes/user-route.js
@@ -1,8 +1,11 @@
 import express from 'express';
 
 import * as UserController from '../controllers/user-controller.js';
+import * as ChefController from '../controllers/chef-controller.js'
 
 const router = express.Router();
+router.route('/')
+.get(ChefController.get)
 router.route('/:id')
       .delete(UserController.deleteUser)
 router.route('/login')
