d3e5e0b46b41a8d94081bbe37fafff8b4d72d98b
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-10 15:02:51 -0500

    2nd statbar added

--- chef-2-go-frontend/src/components/admin/Dashboard.tsx
+++ chef-2-go-frontend/src/components/admin/Dashboard.tsx
@@ -3,8 +3,11 @@ import { ColorModeContext, useMode } from './theme'
 import { Box, Button, IconButton, Typography, useTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { mockTransactions } from './data/mockData';
 import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import Topbar from './menu/Topbar';
 import HeaderAdmin from './menu/HeaderAdmin';
+import StatBox from './menu/StatBox';
 import { tokens } from './theme';
 const Header = HeaderAdmin;
 const Dashboard = () => {
@@ -15,13 +18,13 @@ const Dashboard = () => {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className=''>
-        <main className='flex justify-start'>
+          <main className='flex justify-start'>
             <Topbar />
-        </main>
-        {/* <main className='content flex justify-between'>
+          </main>
+          {/* <main className='content flex justify-between'>
             <SidebarAdmin />
         </main> */}
-      </div>
+        </div>
         <Box m="20px">
           {/* HEADER */}
           <Box display="flex" justifyContent="space-between" alignItems="center">
@@ -40,6 +43,58 @@ const Dashboard = () => {
                 <DownloadOutlinedIcon sx={{ mr: "10px" }} />
                 Download Reports
               </Button>
+            </Box>
+          </Box>
+
+          {/* GRID & CHARTS */}
+          <Box
+            display="grid"
+            gridTemplateColumns="repeat(12, 1fr)"
+            gridAutoRows="140px"
+            gap="20px"
+          >
+            {/* ROW 1 */}
+            <Box
+              sx={{
+                gridColumn: "span 3",
+                backgroundColor: colors.primary[400],
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <StatBox
+                title="12,361"
+                subtitle="Emails Sent"
+                progress="0.75"
+                increase="+14%"
+                icon={
+                  <EmailIcon
+                    sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                  />
+                }
+              />
+            </Box>
+            <Box
+              sx={{
+                gridColumn: "span 3",
+                backgroundColor: colors.primary[400],
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+              }}
+            >
+              <StatBox
+                title="431,225"
+                subtitle="Sales Obtained"
+                progress="0.50"
+                increase="+21%"
+                icon={
+                  <PointOfSaleIcon
+                    sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                  />
+                }
+              />
             </Box>
           </Box>
         </Box>
