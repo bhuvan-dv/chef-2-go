7a85b966dbdbeaa8d52c7f3b8cd133c84a433de5
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-11 03:00:51 -0500

    updated recipe modedl with imageURL attribute

--- chef-2-go-frontend/src/models/Recipe.ts
+++ chef-2-go-frontend/src/models/Recipe.ts
@@ -12,8 +12,9 @@ interface Recipe {
     video?: string;
     gifs?: string;
     comment: string | null;
+    imageUrl:string | undefined
 }
-export const courses: Recipe[] = [
+export const recipes: Recipe[] = [
     {
         comment: null,
         _id: "655cf1d2107b4ea257c79c0b",
@@ -31,7 +32,8 @@ export const courses: Recipe[] = [
             }
         ],
         video: "https://store.com/sajfnkdf",
-        gifs: "https://gifs.com/mygif"
+        gifs: "https://gifs.com/mygif",
+        imageUrl: "https://store.com/sajfnkdf"
     }
 ];
 export default Recipe;
