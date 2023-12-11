1c26b94ad40a58a36b2e3ca418b7d2bf77e26643
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-11 03:02:39 -0500

    Modal and loader changes to Dense Table component

--- chef-2-go-frontend/src/components/Recipe/DenseTable.tsx
+++ chef-2-go-frontend/src/components/Recipe/DenseTable.tsx
@@ -6,55 +6,145 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
-function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number,
+import IngredientsShop from '../../models/ingredientsShop';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import { useState } from 'react';
+import { Button, Modal, Box } from '@mui/material';
+function createIng(
+    id: string,
+    ingName: string,
+    stores: { name: string; location: string }[],
+    // fat: number,
+    // carbs: number,
+    // protein: number,
 ) {
-    return { name, calories, fat, carbs, protein };
+    const store = stores[0];
+    const { name, location } = store;
+    return { id, ingName, shopname: name, location };
 }
 
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
+type IngredientProps = {
+    ingredients: IngredientsShop[] | undefined;
+};
+type RowIngredient = {
+    id: string,
+    ingName: string,
+    shopname: string,
+    location: string
+}
+const rows: RowIngredient[] = [];
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+};
+export default function DenseTable(props: IngredientProps) {
+    if (rows.length <= 0) {
+        props?.ingredients?.forEach((ingObject) => {
+            return rows.push(createIng(ingObject._id, ingObject.name, ingObject.stores));
+        })
+    }
 
-export default function DenseTable() {
+    const handleBtnClick = (location: string) => () => {
+        window.open(location, '_blank'); // Opens the URL in a new tab
+    };
+
+
+    // const [open, setOpen] = useState(false);
+    // const [mapUrl, setMapUrl] = useState('');
+    
+    // function to handle maps for child modal
+    // const handleBtnClick = (location: string) => {
+    //     setMapUrl(location);
+    //     setOpen(true);
+    // };
+
+    // const handleClose = () => {
+    //     setOpen(false);
+    //     setMapUrl('');
+    // };
+
+    // function ChildModal() {
+    //     const [open, setOpen] = React.useState(false);
+    //     const handleOpen = () => {
+    //         setOpen(true);
+    //     };
+    //     const handleClose = () => {
+    //         setOpen(false);
+    //     };
+
+    //     return (
+    //         <React.Fragment>
+    //             <Button onClick={handleOpen}>Open Child Modal</Button>
+    //             <Modal
+    //                 open={open}
+    //                 onClose={handleClose}
+    //                 aria-labelledby="child-modal-title"
+    //                 aria-describedby="child-modal-description"
+    //             >
+    //                 <Box sx={{ ...style, width: 200 }}>
+    //                     <h2 id="child-modal-title">Text in a child modal</h2>
+    //                     <p id="child-modal-description">
+    //                         Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+    //                     </p>
+    //                     <Button onClick={handleClose}>Close Child Modal</Button>
+    //                 </Box>
+    //             </Modal>
+    //         </React.Fragment>
+    //     );
+    // }
+
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650, bgcolor: 'secondary.light', border:'2px solid #38524f'}} size="small" aria-label="a dense table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Ingridents</TableCell>
-                        <TableCell align="right">Retailers</TableCell>
-                        <TableCell align="right">Logo(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {rows.map((row) => (
-                        <TableRow
-                            key={row.name}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650, bgcolor: 'secondary.light', border: '2px solid #38524f' }} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ingridents</TableCell>
+                            {/* <TableCell align="right">Ingredients</TableCell> */}
+                            {/* <TableCell align="right">Logo(g)</TableCell> */}
+                            <TableCell align="right">Retailers&nbsp;(g)</TableCell>
+                            <TableCell align="right">Location&nbsp;(g)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.ingName}
+                                </TableCell>
+                                <TableCell align="right">{row.shopname}</TableCell>
+                                <TableCell align="right">
+                                    <Button
+                                        variant="text"
+                                        onClick={handleBtnClick(row.location)}
+                                        sx={{ color: 'secondary.main', backgroundColor: 'hsl(43, 21%, 94%)' }}
+                                    >
+                                        View Map<FmdGoodIcon />
+                                    </Button>
+                                </TableCell>
+                                {/* <TableCell align="right">{row.calories}</TableCell>
                             <TableCell align="right">{row.fat}</TableCell>
                             <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+                            <TableCell align="right">{row.protein}</TableCell> */}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            {/* <ChildModal /> */}
+            </>
+        
     );
 }
