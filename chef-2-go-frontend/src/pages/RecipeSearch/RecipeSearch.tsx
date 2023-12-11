407fc15eda95ce95b49f6b5dd6331d254759e0af
Keerthana Mikkili <mikkili.k@northeastern.edu>
2023-12-11 01:02:48 -0500

    Added SearchBar in RecipeSearch Page

--- chef-2-go-frontend/src/pages/RecipeSearch/RecipeSearch.tsx
+++ chef-2-go-frontend/src/pages/RecipeSearch/RecipeSearch.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, CardContent, Typography, CardMedia, Grid, Button, TextField } from '@mui/material';
 import { getAllRecipes } from '../../services/recipe';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import RecipeGrid from '../../components/RecipeGrid/RecipeGrid';
 interface Recipe {
   id: number;
   name: string;
@@ -8,8 +10,7 @@ interface Recipe {
   imageUrl: string;
 }
 
-type TopRecipesProps = Recipe[];//= 
-// Recipes: Recipe[]
+type TopRecipesProps = Recipe[];
 
 const RecipeSearch: React.FC = () => {
   const [apiData, setApiData] = React.useState<TopRecipesProps | null>(null);
@@ -27,9 +28,7 @@ const RecipeSearch: React.FC = () => {
   }, [apiData]);
 
   console.log("updated api data", apiData);
-  // console.log(`incoming data: ${Recipes}`);
 
-  // console.log("updated api data",apiData?.Recipes);
   return (
     <div className="container mx-auto my-8">
       {/* Rectangular Full-Width Image Section */}
@@ -41,41 +40,25 @@ const RecipeSearch: React.FC = () => {
         />
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-black">
           <h2 className="text-6xl font-bold mb-46 mr-56">Explore Our Recipes</h2>
+          <br></br>
         </div>
       </div>
-      {/* <section>
-        <div className="container pt-6">
-          <div className="row">
-            <div className="col-sm-12 text-center">
-              <div className="mt-auto">
-                <p className="label font-alt-lite d-inline-block px-3 nice-select-label">Filter Recipes</p>
-                <form className="d-inline-block v-middle">
-                  <select id="filter_product_cats" className="filter-to-page mb-3 mx-1" data-type="recipes_filter" style={{ display: "none;" }}>
-                    <option value="">Filter by Category</option>
-                    <option value="16">Organic Coconut Sweeteners</option><option value="17">Sauces &amp; Marinades</option>        </select><div className="nice-select filter-to-page mb-3 mx-1" tabIndex={0}><span className="current">Filter by Category</span><ul className="list"><li data-value="" className="option selected focus">Filter by Category</li><li data-value="16" className="option">Organic Coconut Sweeteners</li><li data-value="17" className="option">Sauces &amp; Marinades</li></ul></div>
-                  <select id="filter_recipes" className="filter-to-page mb-3 mx-1" data-type="recipes_filter" style={{ display: "none;" }}>
-                    <option value="">Filter by Style</option>
-                    <option value="32">Soups &amp; Salads</option><option value="33">Bowls &amp; Rolls</option><option value="50">Sides &amp; Apps</option><option value="30">Featured</option><option value="35">Baked Goods &amp; Sweets</option><option value="34">Drinks</option><option value="31">Main Dishes</option>             <option value="all">See All</option>
-                  </select><div className="nice-select filter-to-page mb-3 mx-1" tabIndex={0}><span className="current">Filter by Style</span><ul className="list"><li data-value="" className="option selected focus">Filter by Style</li><li data-value="32" className="option">Soups &amp; Salads</li><li data-value="33" className="option">Bowls &amp; Rolls</li><li data-value="50" className="option">Sides &amp; Apps</li><li data-value="30" className="option">Featured</li><li data-value="35" className="option">Baked Goods &amp; Sweets</li><li data-value="34" className="option">Drinks</li><li data-value="31" className="option">Main Dishes</li><li data-value="all" className="option">See All</li></ul></div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
+
+      <SearchBar onSearch={(e) => console.log('search')} />
 
       {/* All Recipes Section */}
       <h1 className="text-3xl font-bold mb-4 mt-8" style={{ color: 'black' }}>All Recipes</h1>
+      {/* <RecipeGrid /> */}
       <Grid container spacing={3}>
         {apiData?.map((Recipe: Recipe) => (
           <Grid item xs={12} sm={6} md={4} key={Recipe.id}>
             <Card>
               <CardMedia
-                    component="img"
-                    height="200"
-                    image={Recipe?.imageUrl}
-                    className="object-cover"
-                  /> 
+                component="img"
+                height="200"
+                image={Recipe?.imageUrl}
+                className="object-cover"
+              />
               <CardContent>
                 <Typography variant="h6">{Recipe.name}</Typography>
                 <Typography variant="h6">{Recipe.chef}</Typography>
