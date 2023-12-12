d129f3f83bb37a59781b3133007b176fdbae4d5f
Shreyas Patil <patil.shreyas@northeastern.edu>
2023-12-12 08:34:58 -0500

    video cards edited for better visual appeal

--- chef-2-go-frontend/src/components/Premium/VideosPage/VideoCard.tsx
+++ chef-2-go-frontend/src/components/Premium/VideosPage/VideoCard.tsx
@@ -12,6 +12,10 @@ const VideoCard: React.FC<VideoCardProps> = ({ videoUrl }) => {
     setIsPlaying(true);
   };
 
+  const handlePause = () => {
+    setIsPlaying(false);
+  };
+
   const videoId = videoUrl.includes('youtu.be')
     ? videoUrl.split('youtu.be/')[1]
     : videoUrl.split('v=')[1];
@@ -19,31 +23,55 @@ const VideoCard: React.FC<VideoCardProps> = ({ videoUrl }) => {
   const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
 
   return (
-    <div className="bg-white overflow-hidden rounded-lg shadow-lg">
-      <div className="cursor-pointer">
+    <div className='m-5'>
+    <div className="bg-gray-100 overflow-hidden rounded-lg shadow-lg group transition-transform transform hover:scale-105">
+      <div className="cursor-pointer relative">
         {!isPlaying && (
-          <img
-            src={thumbnailUrl}
-            alt="Video Thumbnail"
-            className="object-cover w-full h-48"
-            onClick={handlePlay}
-          />
+          <div className="relative">
+            <img
+              src={thumbnailUrl}
+              alt="Video Thumbnail"
+              className="object-cover w-full h-48 transition-opacity duration-300 group-hover:opacity-75 rounded-t-lg"
+              onClick={handlePlay}
+            />
+            <div className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 group-hover:opacity-75`}>
+              <div className="bg-gray-800 text-white hover:bg-gray-700 hover:text-gray-200 transition duration-300 ease-in-out rounded-full p-3 cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="w-8 h-8"
+                  onClick={handlePlay}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 12M6 6l12 6"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
         )}
         {isPlaying && (
           <ReactPlayer
             url={videoUrl}
-            controls
+            controls={true}
             width="100%"
             height="100%"
-            playing
-            onEnded={() => setIsPlaying(false)}
+            playing={true}
+            onEnded={handlePause}
           />
         )}
       </div>
       <div className="p-4">
-        <p className="text-xl font-semibold mb-2">Video Title</p>
-        <p className="text-gray-700">{/* Additional video information */}</p>
+        <p className="text-xl font-Morion font-semi-bold  text-gray-800 mb-2">Video Title</p>
+        <p className="text-gray-600">{/* Additional video information */}</p>
+        {/* <button className='bg-dark-green p-3 rounded-md text-white hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 hover:text-white hover:font-semibold transition animate-ease-in'> Watch Now</button> */}
       </div>
+    </div>
     </div>
   );
 };
--- chef-2-go-frontend/src/pages/VideosPage/Videos.tsx
+++ chef-2-go-frontend/src/pages/VideosPage/Videos.tsx
@@ -6,23 +6,33 @@ import React from 'react';
 const VideosPage: React.FC = () => {
     console.log('Rendering VideosPage');
   const videoUrls = [
-    'https://youtu.be/IEfQ7kyXEYU',
-    'https://youtu.be/DVe-yAE3nAE',
-    'https://youtu.be/onQ9KGWysQY?si=NxJXrsxtICpY3v1F',
-    'https://youtu.be/yJnKIEogUbM?si=c5wCcuE_OGZsSS1-',
-    'https://youtu.be/9WYFcz74Y-M?si=pSJOLAX2r927mMtA',
-    'https://youtu.be/yJnKIEogUbM?si=c5wCcuE_OGZsSS1-',
-    'https://youtu.be/yJnKIEogUbM?si=c5wCcuE_OGZsSS1-',
-    'https://youtu.be/yJnKIEogUbM?si=c5wCcuE_OGZsSS1-',
+    'https://youtu.be/XlKe9MntN30',
+    'https://youtu.be/SU_jumjL8qg',
+    'https://youtu.be/hrCS2Se0ZcU',
+    'https://youtu.be/S_GDu7PcROY',
+    'https://youtu.be/XMnYYhIorbA',
+    'https://youtu.be/XlKe9MntN30',
+    'https://youtu.be/S_GDu7PcROY',
+    'https://youtu.be/SU_jumjL8qg',
 
   ];
 
   return (
-    <div>
+    <div className='bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-200 via-gray-300 to-gray-100'>
       {/* <NavBar /> */}
-      <div className="container mx-auto my-8">
-        <VideoList videoUrls={videoUrls} />
+      {/* <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56"> */}
+
+        <div className="text-center pt-2">
+        <h1 className="text-4xl font-bold tracking-tight font-Morion text-gray-900 sm:text-6xl">Welcome to Chef2Go <button className='bg-dark-green p-3 rounded-xl text-white hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 hover:text-white hover:font-semibold transition animate-ease-in'>Premium </button>.</h1>
+        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>   
       </div>
+      <div className=''>
+        Welcome to Cheftogo Premium
+      </div>
+      <div className="container mx-auto my-8 p-10">
+        <VideoList videoUrls={videoUrls}  />
+      </div>
+   
     </div>
   );
 };
