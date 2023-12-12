cbf5b175e684e6fe47413b342503d5f1bf6779c3
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-12 03:08:51 -0500

    Updated Page not found component

--- chef-2-go-frontend/src/pages/PageNotFound/NotFound.tsx
+++ chef-2-go-frontend/src/pages/PageNotFound/NotFound.tsx
@@ -1,11 +1,17 @@
-import {ReactElement} from "react";
-
-
-const NotFound = ( ): ReactElement => {
+import { ReactElement } from "react";
+import WomenCook from "../../components/loader/WomenCook";
+import { Typography, Paper, Card } from '@mui/material';
+const NotFound = (): ReactElement => {
     return (
-        <>
-            <h1>Page Not Found</h1>
-        </>
+        <div style={{ width: "100%" }}>
+            <Paper elevation={3} sx={{ height: "75vh", width: "75vw", display: "flex", flexDirection: "column", margin: "5vh auto", flexBasis: "80%", bgcolor: "hsl(43, 21%, 94%)" }}>
+                <WomenCook />
+                <Paper elevation={3} sx={{ display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", bgcolor:"hsl(43, 21%, 94%)" }}>
+                    <Typography variant="h2" color={"#38524f"}>What's Cooking Today?</Typography>
+                    <Typography variant="h6" color={"#38524f"}>Page Not Found</Typography>
+                </Paper>
+            </Paper>
+        </div>
     );
 }
 
