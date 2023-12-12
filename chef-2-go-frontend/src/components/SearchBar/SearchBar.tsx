298e22a62bf1f683781f527e52649b317cc7414e
Keerthana Mikkili <mikkili.k@northeastern.edu>
2023-12-11 20:32:05 -0500

    Added Styling to SearchBar Component

--- chef-2-go-frontend/src/components/SearchBar/SearchBar.tsx
+++ chef-2-go-frontend/src/components/SearchBar/SearchBar.tsx
@@ -26,9 +26,9 @@ const SearchBar = (props: SearchBarProps) => {
         // You can also perform additional actions here, like fetching data based on the search term.
     };
     return (
-        <Container sx={{ mb: 2 }}>
-            <TextField variant="outlined" size="small" sx={{ mr: 1 }} onChange={handleInputChange} value={searchInput}></TextField>
-            <Button variant="contained" onClick={handleSearch}>
+        <Container sx={{ mb: 2, display: 'flex', alignItems: 'center', padding: '16px' }}>
+            <TextField variant="outlined" size="small" sx={{ mr: 1, flex: 1, width: '400px', fontSize: '16px' }} onChange={handleInputChange} value={searchInput} placeholder="Enter Recipe Name"></TextField>
+            <Button variant="contained" sx={{ fontSize: '16px', height: '40px',backgroundColor: '#4CAF50', color: '#ffffff', '&:hover': { backgroundColor: '#45a049' }}} onClick={handleSearch}>
                 Search
             </Button>
         </Container>
