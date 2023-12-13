a2cf2188d33b0958dca23a6279c3dc8c08e04813
Basavaraj Patil <patil.basava@outlook.com>
2023-12-13 16:16:23 -0500

    added cracker animation

--- chef-2-go-frontend/src/components/Header/Header.tsx
+++ chef-2-go-frontend/src/components/Header/Header.tsx
@@ -1,19 +1,29 @@
 import React, { FC } from 'react';
 import { Link } from 'react-router-dom';
 import Loader from '../loader/Loader';
+import Confetti from 'react-confetti'
+
 interface HeaderProps {
   menuState: boolean;
   setMenuState: (state: boolean) => void;
 }
 
 const Header: FC<HeaderProps> = ({ menuState, setMenuState }) => {
+  const [playAnimation, setPlayAnimation] = React.useState(false);
+  
   return (
     <header className="site-header">
-      <div className="brand-icon">
+      <div className="brand-icon" onMouseEnter={() => setPlayAnimation(true)} onMouseLeave={() => setPlayAnimation(false)}>
         <Link to="/">
           <div className="icon">
             {/* <i className="fab fa-react"></i> */}
-            <Loader/>
+            <Loader />
+            {playAnimation &&
+            <Confetti
+              width={200}
+              height={50}
+            />
+            }
           </div>
           <span className="text-3xl">CHEF2GO</span>
         </Link>
