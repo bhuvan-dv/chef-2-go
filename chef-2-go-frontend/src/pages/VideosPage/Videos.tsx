ecf9dbcbef72615edb4e64168418b6b18459737f
Shreyas Patil <patil.shreyas@northeastern.edu>
2023-12-13 08:51:24 -0500

    fixed search bar and added styling

--- chef-2-go-frontend/src/pages/VideosPage/Videos.tsx
+++ chef-2-go-frontend/src/pages/VideosPage/Videos.tsx
@@ -1,5 +1,5 @@
 // VideosPage.tsx
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import VideoList from '../../components/Premium/VideosPage/VideoList';
 import VideoCard from '../../components/Premium/VideosPage/VideoCard';
 import SearchIcon from '@mui/icons-material/Search';
@@ -18,13 +18,13 @@ const VideosPage: React.FC = () => {
     { videoUrl: 'https://youtu.be/XlKe9MntN30', title: 'Tasty Egg Fried Rice - Amit (30mins)' },
 
   ];
-
   const [videos, setVideos] = useState(initialVideos);
   const [searchQuery, setSearchQuery] = useState('');
   const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [isSearchSticky, setIsSearchSticky] = useState(false);
 
   const handleSearch = () => {
-    const filteredVideos = initialVideos.filter(video =>
+    const filteredVideos = initialVideos.filter((video) =>
       video.title.toLowerCase().includes(searchQuery.toLowerCase())
     );
     setVideos(filteredVideos);
@@ -39,9 +39,25 @@ const VideosPage: React.FC = () => {
     setIsSearchOpen(false);
   };
 
+  const handleScroll = () => {
+    const scrollPosition = window.scrollY;
+    setIsSearchSticky(scrollPosition > 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-gray-200 via-gray-300 to-gray-100">
-      <div className="text-center pt-2 relative">
+      <div
+        className={`text-center pt-2 relative transition ${
+          isSearchSticky ? 'fixed top-0 left-0 right-0 z-20' : ''
+        }`}
+      >
         <h1 className="text-4xl font-bold tracking-tight font-Morion text-gray-900 sm:text-6xl">
           Welcome to{' '}
           <button className="bg-dark-green p-3 rounded-xl text-white hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 hover:text-white hover:font-semibold transition animate-ease-in">
@@ -54,37 +70,41 @@ const VideosPage: React.FC = () => {
         {isSearchOpen && (
           <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
             <div className="bg-white p-4 rounded-md shadow-md w-96">
-              <input
-                type="text"
-                value={searchQuery}
-                onChange={e => setSearchQuery(e.target.value)}
-                placeholder="Search for videos..."
-                className="w-full border-b-2 border-gray-300 focus:outline-none py-2 px-4 mb-4"
-              />
-              <button
-                onClick={handleSearch}
-                className="bg-dark-green text-white py-2 px-4 rounded-full hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 hover:text-white hover:font-semibold transition animate-ease-in"
-              >
-                Search
-              </button>
+              <div className="relative flex items-center">
+                <input
+                  type="text"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                  placeholder="Search for videos..."
+                  className="w-full border-2 border-gray-300 rounded-full py-2 px-4 pr-12 focus:outline-none shadow-md"
+                />
+                <button
+                  onClick={handleSearch}
+                  className="absolute right-0 top-0 h-full px-3 py-2 bg-dark-green text-white rounded-full hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 hover:text-white hover:font-semibold transition animate-ease-in"
+                >
+                  <SearchIcon />
+                </button>
+              </div>
             </div>
           </div>
         )}
         <div className="flex items-center justify-center mt-4">
           <div className="relative w-72">
-            <input
-              type="text"
-              value={searchQuery}
-              onChange={e => setSearchQuery(e.target.value)}
-              placeholder="Search for videos..."
-              className="w-full border-b-2 border-gray-300 focus:outline-none py-2 px-4 mb-2"
-            />
-            <button
-              onClick={handleSearch}
-              className="absolute right-0 top-0 h-full px-3 py-2 bg-dark-green text-white rounded-full hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 hover:text-white hover:font-semibold transition animate-ease-in"
-            >
-              <SearchIcon />
-            </button>
+            <div className="relative flex items-center">
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                placeholder="Search for videos..."
+                className="w-full border-2 border-gray-300 rounded-full py-2 px-4 pr-12 focus:outline-none shadow-md"
+              />
+              <button
+                onClick={handleSearch}
+                className="absolute right-0 top-0 h-full px-3 py-2 bg-dark-green text-white rounded-full hover:bg-gradient-to-r hover:from-green-400 hover:to-blue-500 hover:text-white hover:font-semibold transition animate-ease-in"
+              >
+                <SearchIcon />
+              </button>
+            </div>
           </div>
         </div>
       </div>
