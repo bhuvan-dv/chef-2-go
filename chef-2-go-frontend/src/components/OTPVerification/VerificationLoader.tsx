198e1594decb5d1b0dc074cdaf10546c986f42d7
D V Bhuvan <damavenkateshraj.b@northeastern.edu>
2023-12-13 12:00:39 -0500

    Verification Animation integrated as a component

---
+++ chef-2-go-frontend/src/components/OTPVerification/VerificationLoader.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Lottie from 'lottie-react';
+import animationData from './Animation - 1702480562531.json';
+const VerificationLoader = () => {
+    const [screenDimensions, setScreenDimensions] = useState({ width: 0, height: 0 });
+
+    useEffect(() => {
+        const updateDimensions = () => {
+            setScreenDimensions({ width: window.innerWidth, height: window.innerHeight });
+        };
+
+        window.addEventListener('resize', updateDimensions);
+        updateDimensions(); // Initialize with current dimensions
+
+        return () => {
+            window.removeEventListener('resize', updateDimensions);
+        };
+    }, []);
+
+    return (
+        <div className='lottieContainer flex justify-center align-middle' style={{ height: 0.7 * screenDimensions.height, width: '50%', margin: "auto 0px" }} >
+            <Lottie animationData={animationData} />
+        </div>
+    );
+}
+
+export default VerificationLoader
